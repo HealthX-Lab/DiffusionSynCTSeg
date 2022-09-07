3388faccfa7662ccdff623cf870fc8fda4f3ab45
Reihaneh <reihane.teimoori@gmail.com>
2022-09-07 11:56:59 -0700

    change image_model.py

--- src/image_model.py
+++ src/image_model.py
@@ -4,15 +4,16 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 import ants
-@gin.configurable
+
 class Image:
     def __init__(self, dic_dir_to_images):
         self.dic_dir_to_images = dic_dir_to_images
-        self.image_dic = {k: None for k, v in dic_dir_to_images}
+        self.image_dic = {k: None for k in dic_dir_to_images}
         self.read_images()
 
     def read_images(self):
-        for dataset_names , path_to_images in self.dic_dir_to_images:
+        for dataset_names, path_to_images in self.dic_dir_to_images.items():
+            paths_tmp = [img_path for img_path in path_to_images]
             self.image_dic[dataset_names] = [(nib.load(img_path)).get_fdata() for img_path in path_to_images]
             print(np.shape(self.image_dic))
     # def read_nii(self):
