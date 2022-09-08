109b611beefbcd2cbb4ab32d035e0545bd9deda3
Reihaneh <reihane.teimoori@gmail.com>
2022-09-08 11:10:41 -0700

    fix path problem

--- src/datasetscollection_model.py
+++ src/datasetscollection_model.py
@@ -10,12 +10,19 @@ class DatasetsCollection:
         self.directory = dir_data
         self.attributes = attributes
         self.names = [names for names in self.attributes]
+        self.path = {names: None for names in self.attributes}
         self.load_datasets()
+
 
     def load_datasets(self):
         for name in self.names:
             dataset_class = eval(self.attributes[name]['class'])
-            dataset_class(name, self.directory, self.attributes[name])
+            class_obj = dataset_class(name, self.directory, self.attributes[name])
+            self.path[name] = class_obj.get_path_to_images()
+
+    def get_paths(self):
+        return self.path
+
 
 
 
