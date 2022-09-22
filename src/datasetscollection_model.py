444d68cd795f361336ccf63dbb74f5520845941a
Reihaneh <reihane.teimoori@gmail.com>
2022-09-22 12:18:32 -0700

    add neuromprphometrics dataset

--- src/datasetscollection_model.py
+++ src/datasetscollection_model.py
@@ -3,6 +3,7 @@ import os
 import sys
 from src.NonContrastDataLoader_model import NonContrastDataLoader
 from src.iDBCERMEPLoader_model import iDBCERMEPLoader
+from src.neuromorphometrics_annotaion import  NeuroMorphometricAnnotatedDataset
 
 @gin.configurable
 class DatasetsCollection:
