42ba476b9b59f3f5cd8840c7eb7501150963dc21
Reihaneh <reihane.teimoori@gmail.com>
2022-10-27 13:39:25 -0700

    segmentation with the LabelFusion approach(aggregated version)

--- scripts/create_atlas_folder.sh
+++ scripts/create_atlas_folder.sh
@@ -1,6 +1,22 @@
-
-search_dir=/home/reyhan/Projects/domain_adaptation_CTscan/data/OASISLabels
-copy_dir=/home/reyhan/Projects/domain_adaptation_CTscan/data/labelFusion/Atlas
+########################## Create a folder for atlas images and fill it ####################
+search_dir=../data/OASISLabels
+copy_dir=../data/labelFusion/Atlas2
+mkdir -p $copy_dir
+#function copy_file ()
+#{
+#  SUB="$3"
+#  image="$1"
+#  echo $SUB
+#  if [[ $image==*"$SUB"*  ]]
+#		 then
+#		   echo '***'
+#		   echo "$1"
+#		  	cp "$1" "$2"
+#		  	echo '@@@@'
+#
+#	fi
+#	return 1
+#}
 for folder in "$search_dir"/*
  do
 	for image in "$folder"/*
@@ -12,10 +28,11 @@ for folder in "$search_dir"/*
 		  	cp $image "$copy_dir"
 
 		 fi
+
 		 SUB='MR2.nii'
 		 if [[ $image == *"$SUB"*  ]];
 		 then
-		   echo $image
+		   echo '###' $image
 		  	cp $image "$copy_dir"
 
 		 fi
--- scripts/label_fusion.sh
+++ scripts/label_fusion.sh
@@ -21,7 +21,8 @@ for target_image in "$target_image_dir"/*
        label_basename=${label_basename%.nii}
        label_basename="$label_basename$label_prefix"
       label_full_path="$label_atlas_dir/$label_basename"
-#	     echo $label_full_path
+      echo label_full_path
+	     echo $label_full_path
 	     i=$((i+1))
 	     ANTS 3 -m CC[$target_image,$atlas_image,1,2] -i 10x50x50x20 -o sub"$i"-to-t1 -t SyN[0.25] -r Gauss[3,0]
        WarpImageMultiTransform 3 $label_full_path Sub"$i"-to-t1-ventricle-lin.nii.gz -R $target_image sub"$i"-to-t1Warp.nii.gz sub"$i"-to-t1Affine.txt
