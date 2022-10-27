36a5224db5aeb976929d9c442e63a127c5d63ceb
Reihaneh <reihane.teimoori@gmail.com>
2022-10-27 13:40:08 -0700

    segmentation with the LabelFusion approach(aggregated version)

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ from src.datasetscollection_model import DatasetsCollection
 from src.trainingchain_model import TrainingChain
 
 
+
 @gin.configurable("main")
 def main(operation, attributes):
+
     if operation == 'EDA':
         datasets = DatasetsCollection(attributes['EDA'])
         image_obj = Image(datasets.get_paths())
