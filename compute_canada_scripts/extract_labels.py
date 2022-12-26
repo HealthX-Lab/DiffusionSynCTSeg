dcc92db1b35fe90441c93ca1782c8a3aebca309e
Reihaneh <reihane.teimoori@gmail.com>
2022-12-25 16:24:56 -0800

    extract ventricle

--- compute_canada_scripts/extract_labels.py
+++ compute_canada_scripts/extract_labels.py
@@ -3,12 +3,15 @@ import numpy as np
 import os
 
 # folder path
-dir_path = '/home/rtm/scratch/rtm/ms_project/data/projectNeuromorphometrics/OASISLabels'
-save_dir = '/home/rtm/scratch/rtm/data/MedImagepreprocess/ventricle_labels/OASISNeuromorphometrics'
+dir_path = '/home/rtm/data/final_dataset/iDB/label/beast'
+save_dir = '/home/rtm/data/final_dataset/iDB/label/beast_ventricle'
 
 
 for folder in os.listdir(dir_path):
-    path = os.path.join(dir_path, folder,str(folder)+ '_seg.nii')
+    # check if current path is a file
+    path = os.path.join(dir_path, folder,'image','Labels.nii.gz')
+    # path = os.path.join(dir_path, folder,str(folder)+ '_seg.nii')
+
     image = (nib.load(path)).get_fdata()
     arr4 = np.where(image == 4, 50, 0)
     arr11 = np.where(image == 11, 100, 0)
@@ -17,10 +20,10 @@ for folder in os.listdir(dir_path):
     arr51 = np.where(image == 51, 250, 0)
     arr = arr4 + arr11 + arr15 + arr52 + arr51
 
-    new_image = nib.Nifti1Image(arr, np.eye(4))
-    save_path = os.path.join(save_dir, str(folder) + '_seg.nii')
+
+    prefix = 'label_' + str(folder) + '.nii.gz'
+    new_image = nib.Nifti1Image(arr.astype(np.float64), np.eye(4))
+    # save_path = os.path.join(save_dir, folder+'.nii.gz')
+    save_path = os.path.join(save_dir, prefix)
     print('save_path', save_path)
     nib.save(new_image, save_path)
-
-
-
--- compute_canada_scripts/run_extract_labels.sh
+++ compute_canada_scripts/run_extract_labels.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 module load python
 python -V
-mkdir -p '/home/rtm/scratch/rtm/data/MedImagepreprocess/ventricle_labels/'
 
-neurogliaSubmit -I  /home/rtm/deeplearning_gpu.simg -j Regular python  /home/rtm/scratch/rtm/ms_project/domain_adaptation_CTscan/scripts/extractlabels.py
+
+neurogliaSubmit -I  /home/rtm/deeplearning_gpu.simg -j Quick python  /home/rtm/scratch/rtm/ms_project/domain_adaptation_CTscan/scripts/extractlabels.py
