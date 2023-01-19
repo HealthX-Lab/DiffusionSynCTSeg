ca682bc1db507e9c5e9b888dabde3b6684f6ff50
Reihaneh <reihane.teimoori@gmail.com>
2023-01-18 18:40:53 -0800

    preprocess CT and MRI

--- data_preprocessing/run_CTWindowing.sh
+++ data_preprocessing/run_CTWindowing.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
-BASE_PATH=/home/rtm/scratch/rtm/data/final_dataset/iDB/CT/normal_ct
-target_ct_path=( $( ls -a ${BASE_PATH}/*.nii.gz))
+module load python
+python -V
+#mkdir -p '/home/rtm/scratch/rtm/data/MedImagepreprocess/ventricle_labels/'
 
-
-for i in $(seq ${#target_ct_path[@]}); do
-    target_ct=${target_ct_path[i-1]}
-
-    echo $target_ct
-    neurogliaSubmit -I  /project/6055004/tools/singularity/khanlab-neuroglia-dwi-master-v1.4.1.simg   \
-      -j Quick /home/rtm/scratch/rtm/ms_project/domain_adaptation_CTscan/scripts/MedImageProcessing.sh  -w $target_ct
-
-done
+neurogliaSubmit -I  /home/rtm/deeplearning_gpu.simg -j Quick python  /home/rtm/scratch/rtm/ms_project/domain_adaptation_CTscan/scripts/threshold.py -100 1000
