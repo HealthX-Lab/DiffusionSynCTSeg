579a997d61eac897d9e7e23c59de4589c86aca97
Reihaneh <reihane.teimoori@gmail.com>
2023-01-22 11:33:34 -0800

    fix affine and header

--- data_preprocessing/change_type.py
+++ data_preprocessing/change_type.py
@@ -10,8 +10,9 @@ pre = 'float_'
 image_name = pre + image_name
 save_path = os.path.dirname(path)
 save_path = os.path.join(save_path, image_name)
-image = (nib.load(path)).get_fdata()
+nib_img = nib.load(path)
+image = nib_img.get_fdata()
 image = np.nan_to_num(image)
 image = image.astype(np.float64)
-new_image = nib.Nifti1Image(image, np.eye(4))
+new_image = nib.Nifti1Image(image,  affine=nib_img.affine,header=nib_img.header)
 nib.save(new_image, save_path)
