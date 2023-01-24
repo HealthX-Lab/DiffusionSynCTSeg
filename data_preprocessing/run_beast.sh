2141d2d5a512122214ac0eb3d07b8600abe4db79
Reihaneh <reihane.teimoori@gmail.com>
2023-01-23 20:21:46 -0800

    change atlas to mri

--- data_preprocessing/run_beast.sh
+++ data_preprocessing/run_beast.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 BASE_PATH=/home/rtm/scratch/rtm/data/final_dataset/Neuromorphometrics/with_Skull
-atlas_path=( $( ls -a ${BASE_PATH}/*.nii.gz))
+mri_path=( $( ls -a ${BASE_PATH}/*.nii.gz))
 
 
-for i in $(seq ${#atlas_path[@]}); do
-    atlas=${atlas_path[i-1]}
-    mri_input_basename="$(basename -- $atlas)"
+for i in $(seq ${#mri_path[@]}); do
+    mri=${mri_path[i-1]}
+    mri_input_basename="$(basename -- $mri)"
     echo 'mri_input_basename in run' $mri_input_basename
     neurogliaSubmit -I  /project/6055004/tools/singularity/minc-toolkit-1.9.16-min.simg   \
-      -j 2core8gb /home/rtm/scratch/rtm/ms_project/domain_adaptation_CTscan/scripts/MedImageProcessing.sh  -b $atlas
+      -j 2core8gb /home/rtm/scratch/rtm/ms_project/domain_adaptation_CTscan/scripts/MedImageProcessing.sh  -b $mri
 
 
 done
